c796711e467d5402785e6fa926c063c04a8c494b
Tsel <thomas.selhorst@online.de>
2015-06-25 07:36:34 +0200

    Initial commit

---
+++ Tracing.py
@@ -0,0 +1,8 @@
+"""
+This is Tracing.py
+This script offers functions for Contact Tracing.
+Tracing is based on Depth first search algorithms described in networkx
+
+Last modified 25.6.2015
+"""
+__author__ = 'TOSS'
